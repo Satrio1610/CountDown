7740d5d537abe893bf3781d407705062dd77f27a
tio <tio.triatmoko@outlook.com>
2015-06-27 16:03:30 +0800

    Reccreate the entire File due to error from previous version

---
+++ app/src/main/java/com/titaniumstick/countdown/ClockTick.java
@@ -0,0 +1,83 @@
+package com.titaniumstick.countdown;
+
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+
+public class ClockTick extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clock_tick);
+        TextView countDown = (TextView) findViewById(R.id.clock);
+        Intent intent = getIntent();
+        Long time = intent.getLongExtra(MainActivity.SEND_TIME, 0);
+        CountClock cod = new CountClock(time,1000,countDown);
+        cod.start();
+
+
+
+
+    }
+
+    public class CountClock extends CountDownTimer {
+        /**
+         * @param millisInFuture    The number of millis in the future from the call
+         *                          to {@link #start()} until the countdown is done and {@link #onFinish()}
+         *                          is called.
+         * @param countDownInterval The interval along the way to receive
+         *                          {@link #onTick(long)} callbacks.
+         */
+        private TextView text;
+        public CountClock(long millisInFuture, long countDownInterval, TextView ntext) {
+            super(millisInFuture, countDownInterval);
+            text = ntext;
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+            Long hour = millisUntilFinished/3600/1000;
+            Long minute = millisUntilFinished/60/1000%60;
+            Long second = millisUntilFinished/1000%60;
+
+            text.setText(hour + ":" + minute + ":" + second);
+        }
+
+        @Override
+        public void onFinish() {
+            text.setText("DONE!");
+
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_clock_tick, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/titaniumstick/countdown/MainActivity.java
+++ app/src/main/java/com/titaniumstick/countdown/MainActivity.java
@@ -1,37 +1,57 @@
 package com.titaniumstick.countdown;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    public final static String SEND_TIME = "com.titaniumstick.countdown.CLOCK";
-    EditText hourEdit;
-    EditText minuteEdit;
-    EditText secondEdit;
-    Button goFokus;
+public class MainActivity extends AppCompatActivity {
+    public final static String SEND_TIME = "com.titaniumstick.countdownver2.TIME";
+    TextView title;
+    EditText hourSet;
+    EditText minuteSet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        // instantiate all the necessary viewParts!
-        hourEdit = (EditText) findViewById(R.id.hour);
-        minuteEdit = (EditText) findViewById(R.id.minute);
-        secondEdit = (EditText) findViewById(R.id.second);
-        goFokus = (Button) findViewById(R.id.goFocus);
-        goFokus.setOnClickListener(this);
-
-
         setContentView(R.layout.activity_main);
-    }
 
+
+    }
+    public void focusGo(View view){
+        title = (TextView) findViewById(R.id.title);
+        hourSet = (EditText) findViewById(R.id.hour);
+        minuteSet = (EditText) findViewById(R.id.minute);
+        String hInput = hourSet.getText().toString();
+        String mInput = minuteSet.getText().toString();
+
+        if( ( hInput.equals("") && mInput.equals(""))
+                || ((Integer.valueOf(hInput)== 0) && (Integer.valueOf(mInput)==0))) {
+            //check if there is no time set or if time set is 00:00
+            title.setText("NO TIME SET!");
+            title.setTypeface(null, Typeface.BOLD);
+            hourSet.setHintTextColor(Color.parseColor("#FF0000"));
+            hourSet.setText("");
+            minuteSet.setHintTextColor(Color.parseColor("#ff0000"));
+            minuteSet.setText("");
+        } else {
+            Intent intent = new Intent(this,ClockTick.class);
+            // processing number of hour written
+            Long nHour = Long.valueOf(hInput)*3600;
+            Long nMin = Long.valueOf(mInput)*60;
+            Long total = (nMin + nHour)*1000;
+            intent.putExtra(SEND_TIME,total);
+            startActivity(intent);
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -53,13 +73,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         return super.onOptionsItemSelected(item);
     }
-
-    @Override
-    public void onClick(View v) {
-        Intent startTime = new Intent(this,CountDownActivity.class);
-        Long number = (Long.valueOf(hourEdit.getText().toString())*3600 + Long.valueOf(minuteEdit.getText().toString())*60 + Long.valueOf(secondEdit.getText().toString()))*1000;
-        startTime.putExtra(SEND_TIME,number);
-        startActivity(startTime);
-
-    }
 }
+
